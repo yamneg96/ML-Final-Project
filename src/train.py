3f4486c29829300b0f4480d069f0c490f7d35f66
Shumye Ayalneh <shmuye27@gmail.com>
2026-01-31 23:34:06 +0300

    add logistic regression training function

--- src/train.py
+++ src/train.py
@@ -3,3 +3,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 
+MODEL_DIR = "models/"
+
+def train_logistic(X_train, y_train):
+    """Train Logistic Regression model."""
+    model = LogisticRegression(
+        class_weight='balanced',
+        max_iter=1000,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+    return model
